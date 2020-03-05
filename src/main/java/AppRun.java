3487d92939055df93a34f1efa821ac7675a68aab
EVonarx <48631478+EVonarx@users.noreply.github.com>
2020-03-05 15:32:47 +0100

    first commit

---
+++ src/main/java/AppRun.java
@@ -0,0 +1,11 @@
+import com.github.evonarx.App;
+
+public class AppRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		App app = new App();
+		System.out.println(app.getMessage2());
+	}
+
+}
